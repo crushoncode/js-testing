583cf7ed2a385e34c3412a325d3b08c4abb66199
Serina Ko <serinako08@gmail.com>
2018-11-18 22:35:43 +1100

    do refactoring from promise to async await

--- functions.test.js
+++ functions.test.js
@@ -45,9 +45,18 @@ test('Admin should be in usernames', () => {
 });
 
 // Working with async data
-test('User fetched name should be Leanne Graham', () => {
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
+// });
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
   expect.assertions(1);
-  return functions.fetchUser().then((data) => {
-    expect(data.name).toEqual('Leanne Graham');
-  });
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
 });
