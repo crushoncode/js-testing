b44005e4f439bc42ac34e7a3d810753474f90970
Serina Ko <serinako08@gmail.com>
2018-11-18 18:06:37 +1100

    do some more basic testing

--- functions.test.js
+++ functions.test.js
@@ -25,3 +25,21 @@ test('User should be Serina Ko object', () => {
     lastName: 'Ko'
   });
 });
+
+// Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
