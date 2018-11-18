170b3edab69c9b0cdcbd6948b8a13e7598a3f9a1
Serina Ko <serinako08@gmail.com>
2018-11-18 17:47:08 +1100

    use 'toEqual' to test for object/array equality

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
-  isNull: () => null
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: 'Serina' };
+    user['lastName'] = 'Ko';
+    return user;
+  }
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -11,3 +11,17 @@ test('Adds 2 + 2 to not equal 5', () => {
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+// toBe is for number and string. It is not going to work on objects.
+// use 'toEqual' to test for object/array equality
+test('User should be Serina Ko object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Serina',
+    lastName: 'Ko'
+  });
+});
